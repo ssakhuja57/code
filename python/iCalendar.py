300ba4cec82659d3dce1c91ca1bcb470265d7b31
ssakhuja57 <ssakhuja57@gmail.com>
2014-03-12 11:22:56 -0400

    cleanup

--- python/iCalendar.py
+++ python/iCalendar.py
@@ -12,13 +12,8 @@ for comp in cal.walk():
 		print comp['dtstamp'].dt
 f.close()
 
-<<<<<<< HEAD
-# use below command to get icalendar from confluence:
 
-# curl -G -k https://username:password@wiki.hq.boston-engineering.com/rest/calendar-services/1.0/calendar/export/subcalendar/b46402cc-44c8-42da-866b-39881d278506.ics?os_authType=basic
-=======
 # use below command to get icalendar from URL:
 
 # curl -G -k https://username:password@address_of_iCalender
 
->>>>>>> cfd9833df39116f351a1a4e363e5358d1ea771a4
