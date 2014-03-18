371cb05e0bf0fa637cde524ca05af4d4257d0bc4
ssakhuja57 <ssakhuja57@gmail.com>
2014-03-18 10:12:00 -0400

    Update extract_revision_scheme.sh

--- bash/extract_revision_scheme.sh
+++ bash/extract_revision_scheme.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# This simply takes a file-based revision scheme in the form of '<value.*>number/char</value>' and strips out the number/char from each line.
+# It assumes that the source file is named revision_scheme_raw.txt and has UNIX format EOL characters and it will create the destination file as revision_scheme.txt
+
 while read LINE 
 do
-	echo "$LINE" | sed -rn 's/<value[^>]*>(.+)<\/value>/\1/p' >> /cygdrive/d/exe007/input/revision_scheme.txt
-done < /cygdrive/d/exe007/input/revision_scheme_raw.txt
-	echo "$LINE" | sed -rn 's/<value[^>]*>(.+)<\/value>/\1/p' >> /cygdrive/d/input/revision_scheme.txt
-done < /cygdrive/d/input/revision_scheme_raw.txt
+	echo "$LINE" | sed -rn 's/<value[^>]*>(.+)<\/value>/\1/p' >> ./revision_scheme.txt
+done < ./revision_scheme_raw.txt
