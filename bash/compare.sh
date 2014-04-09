ced7f2bd3d705355d8df19201f298417487d07d2
ssakhuja57 <ssakhuja57@gmail.com>
2014-04-09 17:23:54 -0400

    Update compare.sh

--- bash/compare.sh
+++ bash/compare.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 # just a reference to follow for comparing strings. IMPORTANT to notice then presence of spaces after/before brackets
 # and after/before the comparators...
@@ -10,3 +9,10 @@ test3=""
 if [ "$test1" == "abc" ] && [ "$test2" == "def" ] && ["$test3" == "" ]; then
   echo "test passed"
 fi
+
+# testing if a var is null, works on numbers, should work on strings too
+
+if [ -z $number123 ]; then
+  echo "variable is null"
+fi
+
